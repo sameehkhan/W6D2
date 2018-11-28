ee450faee8d06e6a905a87e39905f0c6122ee042
Sameeh <sameehkhan1@gmail.com>
2018-11-27 17:06:18 -0800

    Initialize new game in index.js

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -2,5 +2,7 @@ const View = // require appropriate file
 const Game = // require appropriate file
 
   $(() => {
-    // Your code here
+    const rootEl = $('.ttt');
+    const game = new Game();
+    new View(game, rootEl);
   });
